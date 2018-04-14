508644c38ee989146f25c2fa4ec108fec5f735db
Andualem Kelbessa <andualem.u.kelbessa.19@dartmouth.edu>
2018-04-14 17:21:57 -0400

    Adding React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import $ from 'jquery';
+// import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let num = 0;
-setInterval(
-  () => {
-    $('#main').html(`You've been on this page for ${num} seconds`);
-    num += 1;
-  },
-  1000,
-);
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));
+
+// let num = 0;
+// setInterval(
+//   () => {
+//     $('#main').html(`You've been on this page for ${num} seconds`);
+//     num += 1;
+//   },
+//   1000,
+// );
